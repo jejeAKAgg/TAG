2474f2f3fa56725dad83d6ea258f699f6e7a5965
LECHAT Jérôme <62309283+jejeAKAgg@users.noreply.github.com>
2026-02-22 16:52:13 +0100

    feat(battleship): add BattleshipConstants for game state symbols
    - Defined static strings for Water, Ship, Hit, and Miss.
    - Added terrain list for BoardNode initialization.

---
+++ src/main/java/games/BASICbattleship/BattleshipConstants.java
@@ -0,0 +1,18 @@
+package games.BASICbattleship;
+
+import java.util.ArrayList;
+import core.components.BoardNode;
+
+public class BattleshipConstants {
+    public static final String WATER = ".";
+    public static final String SHIP = "S";
+    public static final String MISS = "M";
+    public static final String HIT = "H";
+    
+    public static final ArrayList<BoardNode> terrain = new ArrayList<BoardNode>() {{
+        add(new BoardNode(WATER));
+        add(new BoardNode(SHIP));
+        add(new BoardNode(MISS));
+        add(new BoardNode(HIT));
+    }};
+}
