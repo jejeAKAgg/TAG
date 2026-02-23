efd85fd87dd1e46c7c254ae0eb62b6f8747aa421
LECHAT Jérôme <62309283+jejeAKAgg@users.noreply.github.com>
2026-02-23 13:10:51 +0100

    feat(battleship): add BattleshipGamePhase enum
    - Defined SETUP and PLAY phases for game flow control.

---
+++ src/main/java/games/BESTbattleship/BattleshipGamePhase.java
@@ -0,0 +1,8 @@
+package games.BESTbattleship;
+
+import core.interfaces.IGamePhase;
+
+public enum BattleshipGamePhase implements IGamePhase {
+    SETUP, // Unused (ships are placed automatically in a random manner)
+    PLAY
+}
